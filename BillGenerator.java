891ae75af93e6378bb232e4c95c6b333a2b47567
Arun Sundar V <arunsundar.11113@gmail.com>
2026-02-28 21:10:25 +0530

    day2 questions

---
+++ BillGenerator.java
@@ -0,0 +1,14 @@
+// package qset1;
+import java.util.*;
+
+public class BillGenerator {
+    int getUnits()
+    {
+        int x = 0;
+        Scanner sc = new Scanner(System.in);
+        x = sc.nextInt();
+        return x;
+    }
+
+    
+}
